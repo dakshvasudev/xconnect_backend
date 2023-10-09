480a9f2c2962321515c24d3749f16e1cdc4a88d7
dakshvasudev <vasudevdaksh@gmail.com>
2023-10-09 16:23:43 +0530

    loginController part 5 working

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -37,7 +37,16 @@ class LoginController extends Controller{
             $user_result = DB::table("users")->select("avatar","name","description","type","access_token","token","online")->where("id","=",$user_id)->first();
             return ['code'=>0,'data'=>$user_result,'msg'=>'user has been created'];
         }else{
-            return ['code'=>1,'data'=>$result,'msg'=>'user already exist'];
+            $access_token = md5(uniqid().rand(1000000,9999999));
+            $expire_date = Carbon::now()->addDays(30);
+            DB::table("users")->where($map)->update(
+                [
+                    "access_token"=>$access_token,
+                    "expire_date"=>$expire_date
+                ]
+            );
+            $result->access_token = $access_token;
+            return ['code'=>1,'data'=>$result,'msg'=>'user information updated'];
         }
 
     }
